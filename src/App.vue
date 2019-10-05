c3f5204601c963e6cdc74babca82c82e4dbf12a5
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-10-04 21:54:46 -0600

    Funciones graficadas

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>D'alambert</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="mr-2">Marcela Ixquiac</span>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from './components/HelloWorld.vue';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,198 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="pt-3 text-center">
+      <span class="font-weight-thin display-2">Solución por d'alambert</span>
+    </div>
+
+    <div class="pt-5 text-center">
+      <span class="font-weight-regular headline">Ingrese su función f(x):</span>
+
+      <div class="d-flex justify-center flex-row pt-2 ml-10 mr-10 pl-10 pr-10">
+        <div class="d-flex flex-column mt-2 pt-5">
+          <div class="d-flex" style="width:600px;">
+            <span class="font-weight-light headline mt-3">f(x) =</span>
+            <v-text-field
+              placeholder='Ingrese su función'
+              class="ml-2 font-weight-light"
+              solo
+              clearable
+              color="teal"
+              v-model="funcionF"
+            >
+            </v-text-field>
+          </div>
+          <div class="d-flex" style="width:600px;">
+            <span class="font-weight-light headline mt-3">g(x) =</span>
+            <v-text-field
+              placeholder='Ingrese su función'
+              class="ml-2 font-weight-light"
+              solo
+              clearable
+              color="teal"
+              v-model="funcionG"
+            >
+            </v-text-field>
+          </div>
+        </div>
+        <div class="pl-9 d-flex flex-row align-center">
+          <div class="d-flex flex-column">
+            <v-text-field
+              label="Valor C"
+              outlined
+              v-model="C"
+            ></v-text-field>
+
+            <v-btn
+            rounded
+            color="success"
+            dark
+            class="ml-2 mt-1"
+            v-on:click="f()"
+            >
+              Graficar
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <line-chart
+        :width="400"
+        :height="100"
+        :labels="xInterval"
+        :datasets="displayedDatasets"
+      ></line-chart>
+    </div>
   </div>
 </template>
 
 <script>
+import { evaluate, create, all } from 'mathjs';
+import LineChart from './LineChart.vue';
+
+const config = { };
+const math = create(all, config);
+math.import(require('mathjs-simple-integral'));
+
 export default {
+
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  // datasets,
+  components: {
+    LineChart,
+  },
+  data: () => ({
+    xInterval: [],
+    data: [],
+    funcion: '',
+    funcionF: '',
+    funcionG: '',
+    fIzquierda: '',
+    fDerecha: '',
+    fCombinada: '',
+    C: '',
+    interval: '',
+    contador: 10,
+    selectedFunctions: [],
+    datas: {
+      'funcion f(x)': {
+        label: '',
+        borderColor: 'rgba(50, 115, 220, 0.5)',
+        backgroundColor: 'rgba(50, 115, 220, 0.1)',
+        data: [],
+      },
+      'funcion f(x - ct)': {
+        label: '',
+        borderColor: 'rgba(1, 1, 1, 0.5)',
+        backgroundColor: 'rgba(1, 1, 1, 0.1)',
+        data: [],
+      },
+      'funcion f(x + ct)': {
+        label: '',
+        borderColor: 'rgba(0, 255, 0, 0.5)',
+        backgroundColor: 'rgba(0, 255, 0, 0.1)',
+        data: [],
+      },
+      'funcion combinada': {
+        label: '',
+        borderColor: 'rgba(255, 255, 0, 0.5)',
+        backgroundColor: 'rgba(255, 255, 0, 0.1)',
+        data: [],
+      },
+    },
+  }),
+  methods: {
+    f() {
+      this.xInterval.length = 0;
+      for (let i = 0; i <= 50; i += 1) {
+        this.xInterval.push(i);
+      }
+
+      this.selectedFunctions.length = 0;
+      this.selectedFunctions.push('funcion f(x)');
+
+      this.data = this.xInterval.map(num => this.evaluateFunction(num, this.funcionF));
+      this.datas['funcion f(x)'].data = this.data;
+      this.datas['funcion f(x)'].label = this.funcionF;
+
+      this.desplazamientoIzquierda();
+      this.desplazamientoDerecha();
+
+      this.graficarDesplazamientos();
+      clearInterval(this.interval);
+      this.interval = setInterval(this.graficarDesplazamientos, 1000);
+
+      console.log(math.integral('x^2', 'x'));
+    },
+    evaluateFunction(x, funcion) {
+      const newF = funcion.replace(new RegExp('x', 'g'), x);
+      return evaluate(newF);
+    },
+    desplazamientoIzquierda() {
+      this.fIzquierda = this.funcionF.replace(new RegExp('x', 'g'), `(x + ${this.C} * t)`);
+    },
+    desplazamientoDerecha() {
+      this.fDerecha = this.funcionF.replace(new RegExp('x', 'g'), `(x - ${this.C} * t)`);
+    },
+    graficarDesplazamientos() {
+      const newIzquierda = this.fIzquierda.replace(new RegExp('t', 'g'), this.contador);
+      const newDerecha = this.fDerecha.replace(new RegExp('t', 'g'), this.contador);
+
+      const dataIzq = this.xInterval.map(num => this.evaluateFunction(num, newIzquierda));
+      const dataDer = this.xInterval.map(num => this.evaluateFunction(num, newDerecha));
+
+      const indexIzq = this.selectedFunctions.indexOf('funcion f(x - ct)');
+      if (indexIzq > -1) {
+        this.selectedFunctions.splice(indexIzq, 1);
+      }
+
+      const indexDer = this.selectedFunctions.indexOf('funcion f(x + ct)');
+      if (indexDer > -1) {
+        this.selectedFunctions.splice(indexDer, 1);
+      }
+
+      this.selectedFunctions.push('funcion f(x - ct)');
+      this.selectedFunctions.push('funcion f(x + ct)');
+
+      this.datas['funcion f(x - ct)'].data = dataIzq;
+      this.datas['funcion f(x + ct)'].data = dataDer;
+      this.datas['funcion f(x - ct)'].label = this.fIzquierda;
+      this.datas['funcion f(x + ct)'].label = this.fDerecha;
+
+      this.contador += 1;
+      if (this.contador === 51) {
+        this.contador = 0;
+      }
+    },
+  },
+  computed: {
+    displayedDatasets() {
+      return this.selectedFunctions.map(year => this.datas[year]);
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
